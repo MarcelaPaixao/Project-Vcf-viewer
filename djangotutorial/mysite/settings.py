78b363f8dc197dc68d3ff2c821705550c55b5817
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2026-05-19 21:43:11 -0300

    variantes + login

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -117,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Editar para redirecionar para uma página de home por exemplo
+LOGIN_REDIRECT_URL = "vcf_viewer:index"
--- djangotutorial/vcf_viewer/views.py
+++ djangotutorial/vcf_viewer/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
 from .models import FltrdCybersegChr21Amostras, FltrdCybersegChr21Variantes
+from django.contrib.auth.decorators import login_required
 
-# def index(request):
-#     return HttpResponse("Hello. You're at the vcf_viewer index.")
-
+@login_required
 def index(request):
     # Recupera todos os registros do banco de dados
-    amostras_data = FltrdCybersegChr21Amostras.objects.all()
-    variantes_data = FltrdCybersegChr21Variantes.objects.all()
-    vcf_data = FltrdCybersegChr21Variantes.objects.all()
-
+    # amostras_data = FltrdCybersegChr21Amostras.objects.all()
+    # variantes_data = FltrdCybersegChr21Variantes.objects.all()
+    
     # Passa os dados recuperados para o template
     # context = {
     #     "amostras_data": amostras_data,
     #     "variantes_data": variantes_data
     # }
-    context = {
-        "vcf_data": vcf_data
-    }
+    
+    vcf_data = FltrdCybersegChr21Variantes.objects.all()
+    context = {"vcf_data": vcf_data}
 
     return render(request, "vcf_viewer/index.html", context)
