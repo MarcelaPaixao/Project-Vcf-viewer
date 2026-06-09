09e39d2d183ff4d7c829ae3d2c7256e18e3def15
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2026-06-09 12:01:30 -0300

    amostras table with filters

--- djangotutorial/vcf_viewer/filters.py
+++ djangotutorial/vcf_viewer/filters.py
@@ -11,6 +11,7 @@ class VarianteFilter(django_filters.FilterSet):
             return queryset.filter(**{lookup:False})
         return queryset
     
+    # Método específico para filtrar coluna "Filtro", em que o NULL na verdade pode ser "PASS"
     def filter_not_pass(self, queryset, name, value):
         if value:
             lookup_null_word = '__'.join([name, 'exact'])
@@ -102,7 +103,6 @@ class VarianteFilter(django_filters.FilterSet):
 
     filter__notnull = django_filters.BooleanFilter(
         field_name='filter', 
-        # method='filter_not_null',
         method='filter_not_pass',
         widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
     )
@@ -119,8 +119,8 @@ class VarianteFilter(django_filters.FilterSet):
         widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
     )
     
-    info_db__exact = django_filters.BooleanFilter(
-        field_name='info_db', 
+    info_dp__exact = django_filters.BooleanFilter(
+        field_name='info_dp', 
         method='filter_exact_toggle', 
         widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
     )
@@ -164,20 +164,12 @@ class VarianteFilter(django_filters.FilterSet):
         }
 
 class AmostraFilter(django_filters.FilterSet):
+    
     def filter_not_null(self, queryset, name, value):
         # Só aplica o filtro se a caixa estiver marcada (value == True)
         if value:
             lookup = '__'.join([name, 'isnull'])
             return queryset.filter(**{lookup:False})
-        return queryset
-    
-    def filter_not_pass(self, queryset, name, value):
-        if value:
-            lookup_null_word = '__'.join([name, 'exact'])
-            lookup = '__'.join([name, 'isnull'])
-            return queryset.exclude(
-                Q(**{lookup:True}) |  Q(**{lookup_null_word: "PASS"})
-            )
         return queryset
     
     def filter_exact_toggle(self, queryset, name, value):
@@ -188,119 +180,69 @@ class AmostraFilter(django_filters.FilterSet):
                 return queryset.filter(**{lookup:search_term})
         return queryset
 
-    # id_var__notnull = django_filters.BooleanFilter(
-    #     field_name='id_variante', 
-    #     method='filter_not_null', 
-    #     widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
-    # )
-
-    # id_var__exact = django_filters.BooleanFilter(
-    #     field_name='id_variante', 
-    #     method='filter_exact_toggle', 
-    #     widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
-    # )
-
-    chrom__notnull = django_filters.BooleanFilter(
-        field_name='chrom', 
+    id_variante__notnull = django_filters.BooleanFilter(
+        field_name='id_variante', 
         method='filter_not_null', 
         widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
     )
-    
-    chrom__exact = django_filters.BooleanFilter(
-        field_name='chrom', 
+
+    id_variante__exact = django_filters.BooleanFilter(
+        field_name='id_variante', 
         method='filter_exact_toggle', 
         widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
     )
 
-    pos__notnull = django_filters.BooleanFilter(
-        field_name='pos', 
-        method='filter_not_null', 
-        widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
-    )
-    pos__exact = django_filters.BooleanFilter(
-        field_name='pos', 
-        method='filter_exact_toggle', 
-        widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
-    )
-    
-    ref__notnull = django_filters.BooleanFilter(
-        field_name='ref', 
+    amostra__notnull = django_filters.BooleanFilter(
+        field_name='amostra', 
         method='filter_not_null', 
         widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
     )
     
-    ref__exact = django_filters.BooleanFilter(
-        field_name='ref', 
+    amostra__exact = django_filters.BooleanFilter(
+        field_name='amostra', 
         method='filter_exact_toggle', 
         widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
     )
 
-    alt__notnull = django_filters.BooleanFilter(
-        field_name='alt', 
+    gt__notnull = django_filters.BooleanFilter(
+        field_name='gt', 
+        method='filter_not_null', 
+        widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
+    )
+    gt__exact = django_filters.BooleanFilter(
+        field_name='gt', 
+        method='filter_exact_toggle', 
+        widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
+    )
+    
+    af__notnull = django_filters.BooleanFilter(
+        field_name='af', 
         method='filter_not_null', 
         widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
     )
     
-    alt__exact = django_filters.BooleanFilter(
-        field_name='alt', 
+    af__exact = django_filters.BooleanFilter(
+        field_name='af', 
         method='filter_exact_toggle', 
         widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
     )
 
-    qual__gte = django_filters.NumberFilter(field_name='qual', lookup_expr='gte') ###########################OLHAR!!!
-    qual__notnull = django_filters.BooleanFilter(
-        field_name='qual', 
+    dp__notnull = django_filters.BooleanFilter(
+        field_name='dp', 
         method='filter_not_null', 
         widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
     )
     
-    qual__exact = django_filters.BooleanFilter(
-        field_name='qual', 
-        method='filter_exact_toggle', 
-        widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
-    )
-
-    filter__notnull = django_filters.BooleanFilter(
-        field_name='filter', 
-        # method='filter_not_null',
-        method='filter_not_pass',
-        widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
-    )
-    
-    filter__exact = django_filters.BooleanFilter(
-        field_name='filter', 
-        method='filter_exact_toggle', 
-        widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
-    )
-
-    info_dp__notnull = django_filters.BooleanFilter(
-        field_name='info_dp', 
-        method='filter_not_null', 
-        widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
-    )
-    
-    info_db__exact = django_filters.BooleanFilter(
-        field_name='info_db', 
-        method='filter_exact_toggle', 
-        widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
-    )
-
-    info_gt__notnull = django_filters.BooleanFilter(
-        field_name='info_gt', 
-        method='filter_not_null', 
-        widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
-    )
-    
-    info_gt__exact = django_filters.BooleanFilter(
-        field_name='info_gt', 
+    dp__exact = django_filters.BooleanFilter(
+        field_name='dp', 
         method='filter_exact_toggle', 
         widget=forms.CheckboxInput(attrs={'class': 'hidden-checkbox'})
     )
 
 
     class Meta:
-        model = FltrdCybersegChr21Variantes
-        fields =  ['id_variante', 'chrom', 'pos', 'ref', 'alt', 'qual', 'filter', 'info_dp', 'info_gt']
+        model = FltrdCybersegChr21Amostras
+        fields =  ['id_variante', 'amostra', 'gt', 'af', 'dp']
 
         filter_overrides = {
             models.TextField: {
