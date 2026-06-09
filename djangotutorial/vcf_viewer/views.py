a9e32c8bd43e4507320af39320a257cac5acc1e9
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2026-06-09 12:01:30 -0300

    amostras table with filters

--- djangotutorial/vcf_viewer/views.py
+++ djangotutorial/vcf_viewer/views.py
@@ -1,44 +1,73 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.core.paginator import Paginator
 
 from .models import FltrdCybersegChr21Amostras, FltrdCybersegChr21Variantes
 from django.contrib.auth.decorators import login_required
-from .filters import VarianteFilter
+from .filters import VarianteFilter, AmostraFilter
 
 @login_required
 def variantes(request):
-    # Recupera todos os registros do banco de dados
-    # amostras_data = FltrdCybersegChr21Amostras.objects.all()
-    # variantes_data = FltrdCybersegChr21Variantes.objects.all()
-    
-    # Passa os dados recuperados para o template
-    # context = {
-    #     "amostras_data": amostras_data,
-    #     "variantes_data": variantes_data
-    # }
-    
-    # vcf_data = FltrdCybersegChr21Variantes.objects.all()
-    # context = {"vcf_data": vcf_data}
-    # return render(request, "vcf_viewer/variantes.html", context)
+    queryset = FltrdCybersegChr21Variantes.objects.all()
 
-    f = VarianteFilter(request.GET, queryset=FltrdCybersegChr21Variantes.objects.all())
-    context = {"filter": f}
+    f = VarianteFilter(request.GET, queryset=queryset)
+    
+    paginator = Paginator(f.qs, 100) 
+    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    # Cria uma cópia da URL atual (request.GET)
+    query_params = request.GET.copy()
+    
+    # Remove a páginação da cópia, se ela existir, para não duplicar
+    if 'page' in query_params:
+        del query_params['page']
+
+    context = {
+        "filter": f,
+        "vcf_data": page_obj,
+        # O urlencode transforma o dicionário em texto de URL (ex: "chrom=chr1&qual=50")
+        "query_params": query_params.urlencode(), 
+    }
 
     return render(request, "vcf_viewer/variantes.html", context)
 
 @login_required
-def amostras(request):
-    # Obtém todos os registros
+def amostras1(request):
     queryset = FltrdCybersegChr21Amostras.objects.all()
     
-    # Define o limite de registros por página (ex: 100 por tela)
     paginator = Paginator(queryset, 100) 
     
     # Pega o número da página atual vindo da URL (ex: /amostras?page=2)
     page_number = request.GET.get('page')
     
-    # Extrai apenas as 100 linhas correspondentes àquela página
     vcf_data = paginator.get_page(page_number)
     
-    return render(request, 'pag_amostras.html', {'vcf_data': vcf_data})
+    return render(request, 'vcf_viewer/amostras.html', {'vcf_data': vcf_data})
+
+@login_required
+def amostras(request):
+    queryset = FltrdCybersegChr21Amostras.objects.all()
+
+    f = AmostraFilter(request.GET, queryset=queryset)
+    
+    paginator = Paginator(f.qs, 100) 
+    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    query_params = request.GET.copy()
+
+    if 'page' in query_params:
+        del query_params['page']
+
+    context = {
+        "filter": f,
+        "vcf_data": page_obj,
+        "query_params" : query_params.urlencode()
+    }
+        
+    return render(request, 'vcf_viewer/amostras.html', context)
+
+    # fazer um para o id davariante associado ever de transforma em um linkpara direcionar para outra página 
+    # que vai ter todas as infos sobre essa variante especifica e as amostras relacionadas a ela
