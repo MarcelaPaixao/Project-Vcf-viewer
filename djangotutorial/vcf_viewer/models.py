c8037a6c6c2683cd576ca96e1d8cf3a36075be76
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2026-06-18 12:11:43 -0300

    script original -> django/management

--- djangotutorial/vcf_viewer/models.py
+++ djangotutorial/vcf_viewer/models.py
@@ -21,21 +21,21 @@ class FltrdCybersegChr21Variantes(models.Model):
     info_gt = models.TextField(db_column='INFO_GT', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'Fltrd_Cyberseg_chr21_Variantes'
 
 class FltrdCybersegChr21Amostras(models.Model):
-    id = models.IntegerField(primary_key=True, db_column='rowid')
-
+    # id = models.IntegerField(primary_key=True, db_column='rowid')
     id_variante = models.ForeignKey(FltrdCybersegChr21Variantes, on_delete=models.CASCADE, db_column='ID_VARIANTE')  # Field name made lowercase.
-    # id_variante = models.IntegerField(db_column='ID_VARIANTE', blank=True, null=True)  # Field name made lowercase.
     amostra = models.TextField(db_column='AMOSTRA', blank=True, null=True)  # Field name made lowercase.
     gt = models.TextField(db_column='GT', blank=True, null=True)  # Field name made lowercase.
     af = models.FloatField(db_column='AF', blank=True, null=True)  # Field name made lowercase.
     dp = models.IntegerField(db_column='DP', blank=True, null=True)  # Field name made lowercase.
+    
+    nivel_sigilo = models.IntegerField(db_column='NIVEL_SIGILO', default=1)  # Field name made lowercase.
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'Fltrd_Cyberseg_chr21_Amostras'
 
 
